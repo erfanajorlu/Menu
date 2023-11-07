78a887562564208e14d8b6592d6047d73efa3f93
erfan <erfanajoglu098@gmail.com>
2023-11-07 19:59:01 +0330

    write base of App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Category from "./Category";
+import Menu from "./Menu";
+import data from './data'
 
 function App() {
   return (
-    <div className="">
-        
+    <div className="w-full min-h-screen flex flex-col items-center my-24">
+      <h1 className="text-5xl font-bold text-slate-900">Our Menu</h1>
+      <div className="w-24 h-1 bg-yellow-600 my-4"></div>
+      <Category />
+      <Menu />
     </div>
   );
 }
